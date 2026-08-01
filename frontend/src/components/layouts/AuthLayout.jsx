af1c55ba2a8fa0d60128b42b42a305181b506f6b
Devanshu90 <devanshuaslaliya@gmail.com>
2026-08-01 23:43:32 +0530

    Rename SpendWise to Finance Flow

--- frontend/src/components/layouts/AuthLayout.jsx
+++ frontend/src/components/layouts/AuthLayout.jsx
@@ -7,7 +7,7 @@ const AuthLayout = ({children}) => {
 
         <div className='w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12'>
             <h2 className='text-xl  font-bold text-black'>
-                Spend Wise
+                Finance Flow
             </h2>
             {children}
 
