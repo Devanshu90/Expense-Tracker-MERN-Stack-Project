25c8196891315f125736409836889ddfabb6a97c
Devanshu90 <devanshuaslaliya@gmail.com>
2026-08-23 12:56:43 +0530

    Update currency symbol from $ to ₹ in FinanceOverview and RecentIncomeWithChart components

--- frontend/src/components/Dashboard/RecentIncomeWithChart.jsx
+++ frontend/src/components/Dashboard/RecentIncomeWithChart.jsx
@@ -31,7 +31,7 @@ const RecentIncomeWithChart = ({data, totalIncome}) => {
         <CustomPieChart
             data={chartData}
             label="Total Income"
-            totalAmount={`$${totalIncome}`}
+            totalAmount={`₹${totalIncome}`}
             showTextAnchor
             colors={COLORS}
             />
