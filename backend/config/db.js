db9400ec51b352980d7907987b5e63b6c96ae506
Devanshu90 <devanshuaslaliya@gmail.com>
2026-08-23 13:51:49 +0530

    Refactor database connection setup by adding DNS server configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
+const dns = require("dns");
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {});
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB connected");
-
     } catch (err) {
         console.error("Error connecting to MongoDB", err);
         process.exit(1);
